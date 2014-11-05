392cb60d7ee9f82f02e2375d982cc4379349d8a6
Kamal bin Mustafa <kamal@marimore.co.jp>
2014-11-05 14:51:05 +0900

    add server

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ def hello_world(environ, start_response):
     return ['''Hello %(subject)s
             Hello %(subject)s! '''
             % {'subject': subject}]
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, hello_world)
+    srv.serve_forever()
