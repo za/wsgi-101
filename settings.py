6fadf23374aaf50c9d02f77ba9e02a6ae3567c1a
Zaki Akhmad <za@python.or.id>
2014-11-10 12:31:47 +1100

    still failed

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
 #import os
-import importlib
+#import importlib
+
 
 #BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 #ROOT_URLCONF = os.path.join(BASE_DIR,"urls.py")
 
-#ROOT_URLCONF = 'myapp.urls'
-ROOT_URLCONF = importlib.import_module('urls')
+ROOT_URLCONF = 'myapp.urls'
+#ROOT_URLCONF = importlib.import_module('urls')
