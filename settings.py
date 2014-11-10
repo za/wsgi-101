9a73df339be050f3e352cd2caf3be8bdc829b694
Zaki Akhmad <za@python.or.id>
2014-11-10 11:41:55 +1100

    using importlib module

--- settings.py
+++ settings.py
@@ -6,4 +6,5 @@ import importlib
 #BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 #ROOT_URLCONF = os.path.join(BASE_DIR,"urls.py")
 
-ROOT_URLCONF = 'myapp.urls'
+#ROOT_URLCONF = 'myapp.urls'
+ROOT_URLCONF = importlib.import_module('urls')
